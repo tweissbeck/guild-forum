19c56480a6932fb98a10e24cc1baaaaf09abf44a
tweissbeck <thierry.weissbeck@gmail.com>
2016-08-03 20:44:05 +0200

    Remove the 2nd evolutions. Do not add user at table creation for now as we need to compute some real salt/password hash.

--- conf/evolutions/default/2.sql
+++
@@ -1,8 +0,0 @@
-# --- !Ups
-
-
-INSERT INTO Client (cl_id, cl_lastname, cl_firstname, cl_login, cl_mail, cl_createAt, cl_lastLogIn, cl_admin)
-VALUES (1, 'Admin', 'Right', '@dmin', 'admin@forum.com', NOW(), NULL, TRUE);
-
-# --- !Downs
-DELETE FROM Client;
