736233c1ddb8c1f205806db5927381b4f8ae4edc
tweissbeck <thierry.weissbeck@gmail.com>
2016-08-05 14:46:31 +0200

    Update parser and User class to be compatible with Client sql table

--- app/services/database/UserService.scala
+++ app/services/database/UserService.scala
@@ -13,9 +13,8 @@ import scala.util.Random
 
 object UserService {
 
-
   val userParser = get[Long]("cl_id") ~ get[String]("cl_lastName") ~ get[String]("cl_firstName") ~
-    get[String]("cl_mail") ~ get[Option[String]]("cl_login") ~ get[LocalDateTime]("cl_createAt") ~
+    get[String]("cl_mail") ~ get[Option[String]]("cl_login") ~ get[LocalDateTime]("cl_createdAt") ~
     get[Option[LocalDateTime]]("cl_lastLogin") ~ get[Boolean]("cl_admin") ~ get[String]("cl_password") ~
     get[String]("cl_salt") map {
     case id ~ name ~ firstName ~ mail ~ login ~ createdAt ~ lastLogin ~ admin ~ password ~ salt =>
@@ -46,12 +45,21 @@ object UserService {
     val query =
       s"""
         SELECT * FROM $USER
-        WHERE cl_login = {value} OR cl_email = {value}
+        WHERE cl_login = {value} OR cl_mail = {value}
       """
     SQL(query).on("value" -> value).as(userParser.singleOpt)
   }
 
-  def createUser(data: SignInForm)(implicit c: Connection): User = {
+  def findById(userId: Long)(implicit c: Connection): Option[User] = {
+    val query =
+      s"""
+        SELECT * FROM $USER
+        WHERE cl_id = {id}
+      """
+    SQL(query).on("id" -> userId).as(userParser.singleOpt)
+  }
+
+  def createUser(data: SignInForm, admin: Boolean = false)(implicit c: Connection): User = {
     val createdAt = LocalDateTime.now()
     val salt = Random.alphanumeric.take(50).mkString
     val encryptedSalt = Salt.encrypt(salt, "salt")
@@ -59,15 +67,22 @@ object UserService {
     val query =
       s"""
         INSERT INTO $USER
-        (cl_firstName, cl_lastName, cl_login, cl_mail, cl_password, cl_salt, cl_alias, cl_createdAt)
-        VALUE
-        ({lastName}, {firstName}, {login}, {mail}, {password}, {salt}, {alias})
+        (cl_firstName, cl_lastName, cl_login, cl_mail, cl_password, cl_salt, cl_createdAt, cl_admin)
+        VALUES
+        ({lastName}, {firstName}, {login}, {mail}, {password}, {salt}, {createdAt}, {admin})
       """
-    val userId: Option[Long] = SQL(query).on("lastName" -> data.lastName,
-      "fistName" -> data.firstName,
-      "salt" -> encryptedSalt,
-      "password" -> hashPassword).executeInsert()
-    ???
+    val userId: Option[Long] = SQL(query).
+      on(
+        "lastName" -> data.lastName,
+        "firstName" -> data.firstName,
+        "salt" -> encryptedSalt,
+        "password" -> hashPassword,
+        "createdAt" -> createdAt,
+        "login" -> data.login,
+        "mail" -> data.mail,
+        "admin" -> admin
+      ).executeInsert()
+    findById(userId.get).get
   }
 }
 
