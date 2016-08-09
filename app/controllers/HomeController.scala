1bab9be2aeb305c790a602614c45fe81f7527fc9
tweissbeck <thierry.weissbeck@gmail.com>
2016-08-09 19:48:34 +0200

    Use the new Authenticated action builder in HomeController

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,14 +2,15 @@ package controllers
 
 import javax.inject._
 
+import controllers.composition.Authenticated
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 
 @Singleton
-class HomeController @Inject()(implicit val messagesApi: MessagesApi) extends Controller with I18nSupport {
+class HomeController @Inject()(implicit val messagesApi: MessagesApi, Auth: Authenticated) extends Controller with I18nSupport {
 
-  def index = Action {
-    Ok(views.html.index(None))
+  def index = Auth { implicit request =>
+    Ok(views.html.index(request.user))
   }
 
 
